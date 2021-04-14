6744a5335f9457f6395aa60c4e35464ae3cd2e09
Turonk <gorlov.a@mail.ru>
2021-04-14 10:18:02 +0300

    add tests

---
+++ myprojec/myapp/tests/test_urls.py
@@ -0,0 +1,13 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+
+
+class StaticPagesURLTests(TestCase):
+    def setUp(self):
+        # Создаем неавторизованый клиент
+        self.guest_client = Client()
+
+    def test_about_url_exists_at_desired_location(self):
+        """Проверка доступности главной страницы"""
+        response = self.guest_client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
