42bd82d7c0ae4816d0bbf0f6610893f24fa85157
Turonk <gorlov.a@mail.ru>
2021-04-26 16:21:52 +0300

    tests

---
+++ myprojec/myapp/tests.py
@@ -0,0 +1,23 @@
+from http import HTTPStatus
+from django.test import Client, TestCase
+from django.urls import reverse
+
+
+class StaticPagesURLTests(TestCase):
+    def setUp(self):
+        self.guest_client = Client()
+
+    def test_about_url_exists_at_desired_location(self):
+        """Проверка доступности главной страницы."""
+        response = self.guest_client.get(reverse('myapp:index'))
+        print(response.context)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+        response = self.guest_client.get('/second_page/')
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_page_shows_correct_context(self):
+        """Проверка контекста страниц."""
+        response = self.guest_client.get('/second_page/')
+        print(response)
+        self.assertContains(response, 'А это вторая страница!')
