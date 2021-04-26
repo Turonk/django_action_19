3cd4484599a81fd139a2c23486cf46a559228277
Turonk <gorlov.a@mail.ru>
2021-04-26 16:32:07 +0300

    new tests

--- myprojec/myprojec/urls.py
+++ myprojec/myprojec/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('myapp.urls'), namespace='myapp'),
+    path('', include('myapp.urls', namespace='myapp')),
     path('admin/', admin.site.urls),
 ]
